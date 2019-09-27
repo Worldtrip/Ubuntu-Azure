8791a4022da3614a1a7f7a416ee898ad81701e77
Andrew Brooke <Andrew.Brooke@suffolk.gov.uk>
2019-09-27 11:19:36 +0100

    bug

--- network.tf
+++ network.tf
@@ -1,13 +1,14 @@
 resource "azurerm_virtual_network" "myterraformnetwork" {
     name                = "myVnet"
     address_space       = ["10.0.0.0/16"]
-    location            = "${var.loc}
+    location            = "${var.loc}"
     resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
 
     tags = {
         environment = "Ubuntu Demo"
     }
 }
+
 resource "azurerm_subnet" "myterraformsubnet" {
     name                 = "mySubnet"
     resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
