b41a050761bf4338b3962e281311adbfd27fdf8c
Andrew Brooke <Andrew.Brooke@suffolk.gov.uk>
2019-09-30 16:08:39 +0100

    x

--- vm.tf
+++ vm.tf
@@ -24,13 +24,16 @@ resource "azurerm_virtual_machine" "myterraformvm" {
     location              = "${var.loc}"
     resource_group_name   = "${azurerm_resource_group.nsgs.name}"
     network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
-    vm_size               = "Standard_DS1_v2"
+    #vm_size               = "Standard_DS1_v2"
+    vm_size               = "Standard_B1ls"
+
 
     storage_os_disk {
         name              = "myOsDisk"
         caching           = "ReadWrite"
         create_option     = "FromImage"
         managed_disk_type = "Premium_LRS"
+        #managed_disk_type = "Standard_LRS"
     }
 
     storage_image_reference {
@@ -49,7 +52,8 @@ resource "azurerm_virtual_machine" "myterraformvm" {
         disable_password_authentication = true
         ssh_keys {
             path     = "/home/azureuser/.ssh/authorized_keys"
-            key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAgGoPlW40iMimrPYPBihJTnqt7WgV0KzaPDWwrZeVp+Lbuhd7biM4RAQOuIFErQpMP7YCNME+rBQrHcPqd8SIiUZACaenE1Br5xpMckTnLI3JRGr5pBL6OuQS5y20LDSMuOwIpSqTj43ptCnf6hHpHbrKfnJgp8g4awwUQY3sT8tQ4JpWMbSsmdilfSihOSezUifk5Rf77PzAKelVDBcxQZE6kyw5oX5ubQ4cLHy3w6+th6BMPAhFuCszkakVLbRFUNWVf7CSu8p5dyL3u9xQwmxOeSM3YkLep10i5O7JEeH5jq4LGqJwBsdd+E7zvDVrxvtQ3GWE3A02PS+nNiCHgQ=="
+            key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCe22IyvpC3l1/CWq8BgS78170SgI+4QZ3ZG83fQJcOSws+TXG59yn32uP30j5cQ2CRVyhMEsjuTF/0OAOt6zdRRVhNIlntycRIgy9EdKXWlcrM+UgFnIukQbPnSbaKT3sqDncBRUbzHd3lynZeWNndjFA/id0r9Gly6+PDPNtOIbLVFxraD7q8S73CcfnljqcVxCSKzSVK6skoyopryzTjnzar94L+QgV/5lURFQW900hY0ura08Z4RBUF/bYjjGAJAeG4FmOU+c03OQUjFo7JggFZqYF2iaJT5PQqchkRXrQgGoPZpmFghencdCHTCu68bhP/Xh0tTq5oDVJalEDT"
+            #key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAu+JkHDSwIyMunDF+wtyR+66ClutTBX7xzUbq9yx9fc0WMeKIMTMNlu7GbPJ8B6TCrudB6RVst096SXJ3Uz220Jx7dy8I1peEEXVpn+znro9ygJAaBi5TRE6CCbX6s+FGgbvHq+T+CCh8FkB8eNs9g5Pp4kkOojl760is58ns22mhUC7+MOR8/7xtKjmO7Z89Z1aYPDUzMXax1xZ9jBV1gSfMkXWmFoqXZUdZjMcAEEfg3TbsDAdJvoaSJ/IYBAckWN6zMHmE7TLdijBDPiLBENOhc3mjsKtm/5oRs4xDMPJSx0DcC47W+AH0G2GO+/phHW3JwO1kUgMWpmRB+/emgw=="
         }
     }
     boot_diagnostics {
